2c3d4e493a86dae866f27b33a9a1e1d210a5dc5f
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-04-20 15:52:01 +0300

    add component to quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Quote} from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(1,'Agnes','fly like a butterfly sting like a bee'),
+    new Quote(2,'Liz','be yourself'),
+    new Quote(3, 'Paul','Get a smile'),
+  ]
 
   constructor() { }
 
