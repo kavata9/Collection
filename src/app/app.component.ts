550a7cc42ff06aefab93ffc216dc54a509036370
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-04-20 12:02:42 +0300

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  quotes :string[];
+
+  constructor (){
+    
+    this.quotes =["Fly like a butterfly sting like a bee","am out of here","dancing in the rain"];
+  }
 }
