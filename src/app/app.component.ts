3bcce9e0e18f8ba3dc09bb11bf7036fbe45232df
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-04-20 14:27:28 +0300

    display my quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  quotes :string[];
-
-  constructor (){
-    
-    this.quotes =["Fly like a butterfly sting like a bee","am out of here","dancing in the rain"];
-  }
+  quote = 'fly like a butterfly sting like a bee';
 }
