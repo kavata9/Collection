c9ddb13b4cd0344628590805918af8aaf7aca42c
Agnes Kavata <kavatamusyoka24@gmail.com>
2018-04-21 23:27:39 +0300

    add delete buttor for a user to be able to delete a quote created

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,20 +13,25 @@ export class QuoteComponent implements OnInit {
     new Quote(3, 'Paul','Get a smile',new Date(2018,3,14) ),
   ]
 
-  completeQuote(isComplete,index){
-    if (isComplete){
-      this.quotes.splice(index,1);
-    }
-  
-  }
 
   toogleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
 
+  deleteQuote(isComplete,index){
+    if (isComplete){
+      let toDelete=confirm('Are you sure you want to delete ${this.quotes[index].name}')
+
+      if(toDelete){
+      this.quotes.splice(index,1);
+    }
+  }
+  
+  }
+
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
